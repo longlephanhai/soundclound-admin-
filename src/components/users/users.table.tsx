1ec3da127dad7160308fe92d3ac971261974f0b5
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-26 00:51:09 +0700

    design antd modal

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,5 +1,5 @@
 import { PlusOutlined } from '@ant-design/icons';
-import { Table, Button, type TableProps, Modal } from 'antd';
+import { Table, Button, type TableProps, Modal, Input } from 'antd';
 import { useState } from 'react';
 
 interface IUser {
@@ -10,14 +10,23 @@ interface IUser {
 }
 
 const UsersTable = () => {
-  const data: IUser[] = [
-    {
-      _id: '1',
-      email: 'long@gmail.com',
-      name: 'Long',
-      role: 'Admin',
-    }
-  ]
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [data, setData] = useState<IUser[]>([{
+    _id: '1',
+    email: 'long@gmail.com',
+    name: 'Long',
+    role: 'Admin',
+  }]);
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [age, setAge] = useState<string>('');
+  const [gender, setGender] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
+  const [role, setRole] = useState<string>('');
 
   const columns: TableProps<IUser>['columns'] = [
     {
@@ -37,13 +46,19 @@ const UsersTable = () => {
     }
   ]
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
     setIsModalOpen(true);
   };
 
   const handleOk = () => {
+    setData([...data, {
+      _id: Math.random().toString(36).substring(2, 15), // Generate a random ID
+      email,
+      name,
+      role,
+
+    }])
     setIsModalOpen(false);
   };
 
@@ -81,15 +96,64 @@ const UsersTable = () => {
       />
 
       <Modal
-        title="Basic Modal"
+        title="Add new user"
         closable={{ 'aria-label': 'Custom Close Button' }}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
+        maskClosable={false}
+        okText="Add"
+        cancelText="Cancel"
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <div>
+          <label htmlFor="">Name:</label>
+          <Input
+            value={name}
+            onChange={(e) => { setName(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Email:</label>
+          <Input
+            value={email}
+            onChange={(e) => { setEmail(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Password:</label>
+          <Input
+            value={password}
+            onChange={(e) => { setPassword(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Age:</label>
+          <Input
+            value={age}
+            onChange={(e) => { setAge(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Gender:</label>
+          <Input
+            value={gender}
+            onChange={(e) => { setGender(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Address</label>
+          <Input
+            value={address}
+            onChange={(e) => { setAddress(e.target.value) }}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Role</label>
+          <Input
+            value={role}
+            onChange={(e) => { setRole(e.target.value) }}
+          />
+        </div>
       </Modal>
     </>
   )
