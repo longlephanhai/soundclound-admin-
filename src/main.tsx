aabe3ffddac8a6d5a8351e9f6f37d281b7e92e83
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-25 23:48:50 +0700

    design admin page

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,69 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import {
   createBrowserRouter,
+  Link,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 import UsersPage from './pages/users.page.tsx';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to={'/'}>Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to={'users'}>Manage Users</Link>,
+    key: 'users',
+    icon: <UserOutlined />,
+  },
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState('home');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+  );
+};
+
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <footer>Footer</footer>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <LayoutAdmin />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: 'users',
+        element: <UsersPage />,
+      }
+    ]
   },
-  {
-    path: '/users',
-    element: <UsersPage />,
-  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/users.page.tsx
+++ src/pages/users.page.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react"
 import UsersTable from "../components/users/users.table"
 import '../styles/user.css'
+interface IUser {
+  email: string;
+  name: string;
+  role: string;
+}
 const UsersPage = () => {
+  const [listUsers, setListUsers] = useState<IUser[]>([])
+  useEffect(() => {
+    getData()
+  }, [])
+  const getData = async () => {
+    const res = await fetch('http://localhost:8000/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: "long@gmail.com",
+        password: "12345678"
+      })
+    })
+    const dataRes = await res.json()
+    setListUsers(dataRes)
+  }
   return (
     <>
       <UsersTable />
