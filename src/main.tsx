f637e37c2d8e849d67f16603e0e5869a86c18610
LePhanHaiLong <longlph.23it@gmail.com>
2025-05-25 16:43:34 +0700

    init project

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: '/users',
+    element: <div>manage users</div>,
+  }
+]);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
